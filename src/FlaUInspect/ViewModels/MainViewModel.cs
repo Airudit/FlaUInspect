8130e6ff48fbeae7b8cc7067e69125b05364b51a
SandRock <sandrock@sandrock.fr>
2024-04-04 18:40:33 +0200

    export visual tree to json

--- src/FlaUInspect/ViewModels/MainViewModel.cs
+++ src/FlaUInspect/ViewModels/MainViewModel.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Windows.Input;
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Exceptions;
 using FlaUI.UIA2;
 using FlaUI.UIA3;
 using FlaUInspect.Core;
 using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FlaUInspect.ViewModels
 {
@@ -49,6 +55,10 @@ namespace FlaUInspect.ViewModels
             RefreshCommand = new RelayCommand(o =>
             {
                 RefreshTree();
+            });
+            ExportCommand = new RelayCommand(o =>
+            {
+                ExportTree();
             });
         }
 
@@ -103,6 +113,8 @@ namespace FlaUInspect.ViewModels
         public ICommand CaptureSelectedItemCommand { get; private set; }
 
         public ICommand RefreshCommand { get; private set; }
+
+        public ICommand ExportCommand { get; private set; }
 
         public ObservableCollection<DetailGroupViewModel> SelectedItemDetails => SelectedItemInTree?.ItemDetails;
 
@@ -208,6 +220,98 @@ namespace FlaUInspect.ViewModels
         {
             Elements.Clear();
             Initialize(SelectedAutomationType);
+        }
+
+        private void ExportTree()
+        {
+            var root = new JObject();
+            root["Manifest"] = "FlaUInspect export";
+            root["DateCreatedUtc"] = DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture);
+            var tree = new JObject();
+            root["Tree"] = tree;
+            foreach (var element in Elements)
+            {
+                PopulateJsonTree(tree, element);
+            }
+            
+            // TODO: ask user for file path
+            var filePath = "export.json";
+
+            try
+            {
+                var settings = new JsonSerializerSettings();
+                settings.Formatting = Formatting.Indented;
+                var serializer = JsonSerializer.CreateDefault(settings);
+                using (var file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+                {
+                    file.SetLength(0L);
+                    using (var textWriter = new StreamWriter(file, Encoding.UTF8))
+                    using (var jsonWriter = new JsonTextWriter(textWriter))
+                    {
+                        serializer.Serialize(jsonWriter, root);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private void PopulateJsonTree(JObject node, ElementViewModel element)
+        {
+            node["Name"] = element.Name;
+            PopulateJsonProperty(node, element, "AutomationId", x => x.AutomationId);
+            PopulateJsonProperty(node, element, "ControlType", x => x.ControlType.ToString());
+            PopulateJsonProperty(node, element, "XPath", x => x.XPath);
+
+            if (element.ItemDetails != null && element.ItemDetails.Count > 0)
+            {
+                foreach (var details in element.ItemDetails)
+                {
+                    var detailNode = new JObject();
+                    node[details.Name] = detailNode;
+
+                    if (details.Details != null && details.Details.Count > 0)
+                    {
+                        foreach (var pair in details.Details)
+                        {
+                            detailNode[pair.Key] = pair.Value;
+                        }
+                    }
+                }
+            }
+            
+            if (element.Children != null && element.Children.Count > 0 && element.IsExpanded)
+            {
+                PopulateJsonTree(node, element.Children);
+            }
+        }
+
+        private static void PopulateJsonProperty(JObject node, ElementViewModel element, string key, Func<ElementViewModel, string> func)
+        {
+            try
+            {
+                node[key] = func(element);
+            }
+            catch (PropertyNotSupportedException)
+            {
+            }
+        }
+
+        private void PopulateJsonTree(JObject parent, IList<ElementViewModel> elements)
+        {
+            var parentChildren = new JArray();
+            parent["Children"] = parentChildren;
+            
+            foreach (var element in elements)
+            {
+                var node = new JObject();
+                parentChildren.Add(node);
+
+                PopulateJsonTree(node, element);
+            }
         }
     }
 }
